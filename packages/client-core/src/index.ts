7728be9efa233ae437f5301288c97c606baece53
Youngju Jaden Kim <pydemia@gmail.com>
2026-09-16 10:20:00 +0900

    build: pin alpha.47 for Windows Electron snapshot cleanup

--- packages/client-core/src/index.ts
+++ packages/client-core/src/index.ts
@@ -3,7 +3,7 @@ import { CLIENT_CONTRACT_VERSION, type ClientPackageInfo } from '@gcr/client-con
 
 export const clientCorePackage: ClientPackageInfo = Object.freeze({
   name: '@gcr/client-core',
-  version: '0.1.0-alpha.46',
+  version: '0.1.0-alpha.47',
   contractVersion: CLIENT_CONTRACT_VERSION,
 });
 
